837f042d9349d06c3b6a120ee265574817da0bea
Horace Everett <horace.everett@gmail.com>
2024-10-30 19:42:47 -0400

    exercise 14

--- exercises/14-more-control-flow/02-short-circuiting.js
+++ exercises/14-more-control-flow/02-short-circuiting.js
@@ -11,3 +11,7 @@ let error = "Username already exists."; // When the test runs, this could also b
  */
 
 // WRITE YOUR ANSWER BELOW THIS LINE
+
+
+let errorMessage = error || ""; 
+console.log(errorMessage);
