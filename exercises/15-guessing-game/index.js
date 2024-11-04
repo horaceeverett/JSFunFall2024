df3ae812c810654ff797ce62d2856cc7f714b863
Horace Everett <horace.everett@gmail.com>
2024-11-04 18:42:39 -0500

    Merge branch 'main' of https://github.com/AlbanyCanCodeCourses/JSFunFall2024

--- exercises/15-guessing-game/index.js
+++ exercises/15-guessing-game/index.js
@@ -6,6 +6,6 @@ import prompt from "picoprompt";
 // This pauses and asks the user a question over the command line.
 // The user will type an answer and hit ENTER.
 // What they typed will be stored in the constant "answer".
-const answer = prompt("How are you today?: ");
+const answer = await prompt("How are you today?: ");
 // This prints "answer" on to the screen.
 console.log(answer);
