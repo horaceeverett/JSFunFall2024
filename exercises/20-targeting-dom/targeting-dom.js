c044eb76aa6f3216a5337bdee2ad50bed7fc3c71
Horace Everett <horace.everett@gmail.com>
2024-11-20 19:41:00 -0500

    update to exercise 20 targeting dom

--- exercises/20-targeting-dom/targeting-dom.js
+++ exercises/20-targeting-dom/targeting-dom.js
@@ -1,3 +1,6 @@
+
+
+
 /**
  * You will be targeting DOM elements (the HTML) on the page using
  * "document.querySelector" or "document.querySelectorAll"
@@ -11,5 +14,25 @@
  * Do not change the HTML unless you are instructed to do so.
  */
 (function () {
-  // Put your answers in here
+  console.log("1. Single Elements:");
+  const firstLi = document.querySelector("section ul li");
+  console.log(firstLi);
+
+  const myIdElement = document.querySelector("#myId");
+  console.log(myIdElement);
+
+  console.log("\nGroups of Elements:");
+
+  const yellowElements = document.querySelectorAll(".bg-warning");
+  yellowElements.forEach(el => console.log(el));
+
+  console.log("\nStructuring HTML for JavaScript:");
+
+  const socialLinks = document.querySelectorAll("a.social-link");
+  socialLinks.forEach(link => console.log(link));
+
+  console.log("\nNested Elements:");
+
+  const targetCells = document.querySelectorAll("#myRow .col");
+  targetCells.forEach(cell => console.log(cell));
 })();
